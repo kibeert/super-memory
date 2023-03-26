597c3f5d20c51f95992a95032662ba6e5b9b845d
kibet <107929686+kibeert@users.noreply.github.com>
2023-03-26 09:08:03 -0400

    Add files via upload

---
+++ Try.java
@@ -0,0 +1,12 @@
+public class Try {
+    public static void main(String[] args) {
+        try {
+            int[] Mynumbers = { 1, 2, 3, 4, 5 };
+            System.out.println(Mynumbers[4]);
+        } catch (Exception e) {
+            System.out.println("array not in the array");
+        } finally {
+            System.out.println("the try and catch is finished");
+        }
+    }
+}
